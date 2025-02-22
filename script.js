eb2e0a67297f0cd94f2f2fd850b5033688cb8831
Thekillerk685 <karlobogovic57@gmail.com>
2025-02-22 12:39:53 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.querySelector(".btn");
+    button.addEventListener("click", function() {
+        alert("Welcome to the Glorious Empire!");
+    });
+});
